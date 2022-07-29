391538cc85fc074b8c086f603481d255500541a0
PB2 <nft192891@gmail.com>
2022-07-29 15:38:05 +0800

    完成文章管理的分頁功能

--- api/news.php
+++ api/news.php
@@ -10,11 +10,12 @@ if(isset($_POST['del'])){ // 若它存在 表示有資料要被刪除
 $rows=$News->all(); // 先拿出所有資料
 
 foreach($rows as $row){ // 一筆一筆檢查 檢查id有無傳回 有的話顯示 沒有的話不顯示
-    if(in_array($row['id'],$_POST['sh'])){ // 檢查該陣列row內的id 有無在POSTso4的sh
+    if(in_array($row['id'],$_POST['sh'])){ // 檢查該陣列row內的id 有無在POST內的sh
         $row['sh']=1; // 若有設為1  若無設為0
     }else{
         $row['sh']=0;
     }
+
     $News->save($row);
 }
 to("../back.php?do=news");
--- back/news.php
+++ back/news.php
@@ -1,6 +1,7 @@
 <form action="./api/news.php" method="post">
 <!-- table.tab.cent>tr*2 -->
 <table class="tab cent ct">
+<!-- <table class="width:80%; margin:auto; text-align:center"> -->
     <tr>
         <th width="10%">編號</th>
         <th width="70%">標題</th>
@@ -8,7 +9,19 @@
         <th width="10%">刪除</th>
     </tr>
     <?php
-    $rows=$News->all();
+    // 分頁功能
+    // 這裡的$News 為base.php的DB(資料表)
+    $all=$News->math('count','id'); // 紀錄資料表內的總筆數(假設id有10, 代表總筆數為10)
+    $div=3; // 一頁幾筆
+    $pages=ceil($all/$div); // 頁數=計算(總比數  除  一頁幾筆)
+    $now=$_GET['p']??1; // 辨識自己在哪一頁 預設從1開始
+    $start=($now-1)*$div; // 開始頁=(現在頁數-1)乘3
+    $rows=$News->all("limit $start, $div"); 
+    // rows(陣列)=在該DB資料表使用all-function(條件) 找出全部資料, 再由下方foreach把資料吐出來
+    // rows=顯示哪些資料, limit=指定顯示的欄位數 第一個值=從哪開始 第二個值=顯示幾筆
+    // $rows=$News從索引值開始抓(一頁3筆) // 15:02 
+
+
     foreach($rows as $key=>$row){
     ?>
     <tr>
@@ -23,6 +36,31 @@
     }
     ?>
 </table>
+<!-- 分頁頁碼與符號 -->
+<div class="ct">
+<?php
+// 上一頁
+if(($now-1)>0){
+    echo "<a href='?do=news&p=".($now-1)."'> < </a>";
+}
+// 頁碼
+for($i=1;$i<=$pages;$i++){
+    $fontsize=($now==$i)?'24px':'16px';
+    echo "<a href='?do=news&p=$i' style='font-size:$$fontsize'>";
+    echo $i;
+    echo "</a>";
+}
+// 下一頁
+if(($now+1)<=$pages){
+    echo "<a href='?do=news&p=".($now+1)."'> > </a>";
+}
+
+?>
+<!-- 分頁頁碼與符號 END -->
+
+
+</div>
+
 <div class="ct"><input type="submit" value="確定修改"></div>
 
 </form>
