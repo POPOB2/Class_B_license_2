d2dc0d7db3418903a88122765c35392192f27f4b
PB2 <nft192891@gmail.com>
2022-08-03 16:03:09 +0800

    建立分類網誌基本外觀及點擊功能

--- front/po.php
+++ front/po.php
@@ -1,1 +1,48 @@
-分類網誌
+<!-- 分類網誌 -->
+<style>
+    .type{
+        cursor: pointer; /* 游標:手指 */ 
+        color: blue;
+        margin: 1rem 0 ;
+        /* padding-bottom: 0.5rem; */
+        max-width: max-content;
+    }
+    .type:hover{
+        /* 都可以用 */
+        /* 線離文字近 */
+        /* text-decoration: underline; */
+        /* 線比文字長, 在上方type新增max-width讓線的長度為文字內容最大長度 */
+        border-bottom:  1px solid blue;
+        
+    }
+</style>
+
+<div>目前位置 : 首頁 > 分類網誌 > <span id="header">健康新知</span></div>
+<!-- fieldset 帶有block屬性 所以兩個fieldset會產生斷行效果, 為使fieldset並排有兩種方法1.改為inline 2.賦予寬度 -->
+<!-- 這裡我們新增div包覆legend並賦予display:flex使他並排 -->
+<div style="display:flex">
+<fieldset>
+    <legend>分類網誌</legend>
+    <div class="type">健康新知</div>
+    <div class="type">菸害防制</div>
+    <div class="type">癌症防治</div>
+    <div class="type">慢性病防治</div>
+</fieldset>
+<fieldset>
+    <legend>文章列表</legend>
+    <div></div>
+</fieldset>
+</div>
+
+<script>
+    // 設置("class:type")被開啟("開啟方式為click", 執行function){}
+    $(".type").on("click",function(){
+        // this=當下按的那個標籤, 因為type有很多 這裡使用this來定位是哪個type被點擊
+        // 當上面class:type被點的時候,  要拿到被點的type的文字內容
+        let type=$(this).text()
+        /* 把this取得的type的text 
+           放到 "id:header" 的text(type) <=這玩意兒在最上面的span*/
+        $("#header").text(type);
+        // console.log(type);
+    })
+</script>
